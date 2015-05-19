20ccd44231328cd6490c2558a7ea598f8e34dd89
Matheus Candido <matheus@mcassiano.com>
2015-05-19 09:38:55 -0300

    event info

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var manager;
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  manager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
